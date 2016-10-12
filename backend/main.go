7cda6d166018d3fa297e986926aaaef010e4283b
David Klassen <f0rk.tt@gmail.com>
2016-10-12 16:22:42 +0200

    Refactoring

--- backend/main.go
+++ backend/main.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"flag"
-	_ "github.com/lib/pq"
 	"github.com/satori/go.uuid"
 	"golang.org/x/net/websocket"
 	"io"
@@ -14,72 +10,24 @@ import (
 	"path"
 )
 
-var (
-	port       = flag.String("port", "443", "The application port")
-	storageDir = flag.String("storage", "/recording-storage/", "Path to the storage directory")
-)
-
-var (
-	restURL    = "http://rest/"
-	restClient = http.Client{}
-)
-
-type recording struct {
-	ID       string `json:"id"`
-	FileName string `json:"fileName"`
-}
-
 func serve(ws *websocket.Conn) {
 	// generate new recording id and send to the client
 	id := uuid.NewV4()
-	err := websocket.Message.Send(ws, id.String())
-	check(err)
+	websocket.Message.Send(ws, id.String())
 	log.Printf("Starting recording: %s\n", id.String())
 
 	// open new webm file
-	fileName := path.Join(*storageDir, id.String()+".webm")
-	f, err := os.Create(fileName)
-	check(err)
+	fileName := path.Join("/recording-storage/", id.String()+".webm")
+	f, _ := os.Create(fileName)
 	defer f.Close()
 
-	body, err := json.Marshal(recording{
-		ID:       id.String(),
-		FileName: id.String() + ".webm",
-	})
-	check(err)
-	req, err := http.NewRequest(http.MethodPut, restURL, bytes.NewReader(body))
-	check(err)
-	_, err = restClient.Do(req)
-	check(err)
-
-	// receive chunks until the connection is closed
-	for {
-		var buffer []byte
-		if err := websocket.Message.Receive(ws, &buffer); err != nil {
-			// io.EOF in ws stream means the connection was closed, we can stop the reading loop
-			if err == io.EOF {
-				log.Printf("Done recording: %s\n", id.String())
-				return
-			}
-
-			panic(err)
-		}
-		// append buffer to webm file
-		_, err := f.Write(buffer)
-		check(err)
-	}
+	// stream to file
+	n, _ := io.Copy(f, ws)
+	log.Printf("Done recording: %s; bytes writtes: %d\n", id.String(), n)
 }
 
 func main() {
-	flag.Parse()
-
 	http.Handle("/", websocket.Handler(serve))
-	log.Printf("Starting recording service on port :%s\n", *port)
-	log.Fatal(http.ListenAndServe(":"+*port, nil))
-}
-
-func check(err error) {
-	if err != nil {
-		panic(err)
-	}
+	log.Println("Starting recording service on port 443")
+	log.Fatal(http.ListenAndServe(":443", nil))
 }
--- rest/main.go
+++ rest/main.go
@@ -1,110 +1,22 @@
 package main
 
 import (
-	"database/sql"
-	"encoding/json"
-	"flag"
-	"github.com/gorilla/handlers"
-	_ "github.com/lib/pq"
-	"github.com/rubenv/sql-migrate"
 	"log"
 	"net/http"
-	"os"
-	"time"
+	"io/ioutil"
+	"fmt"
 )
-
-var (
-	port          = flag.String("port", "80", "The application port")
-	dbinfo        = flag.String("dbinfo", "host=recording_db user=postgres password=postgres dbname=postgres sslmode=disable", "Database connection string")
-	migrationsDir = flag.String("migrations", "/migrations/", "Path to the migrations directory")
-)
-
-var db *sql.DB
-
-type recording struct {
-	ID       string `json:"id"`
-	FileName string `json:"fileName"`
-}
 
 func index(w http.ResponseWriter, _ *http.Request) {
-	query := "SELECT id, file_name FROM recordings"
-	rows, err := db.Query(query)
-	check(err)
-
-	res := make([]recording, 0)
-	for rows.Next() {
-		var rec recording
-		err := rows.Scan(&rec.ID, &rec.FileName)
-		check(err)
-		res = append(res, rec)
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	files, _ := ioutil.ReadDir("/storage")
+	for _, f := range files {
+		fmt.Fprintln(w, f.Name())
 	}
-	err = rows.Err()
-	check(err)
-
-	json.NewEncoder(w).Encode(res)
-}
-
-func create(w http.ResponseWriter, r *http.Request) {
-	var rec recording
-	err := json.NewDecoder(r.Body).Decode(&rec)
-	check(err)
-
-	_, err = db.Exec("INSERT INTO recordings (id, file_name) VALUES($1, $2)", rec.ID, rec.FileName)
-	check(err)
-
-	w.WriteHeader(http.StatusCreated)
 }
 
 func main() {
-	flag.Parse()
-
-	var err error
-	db, err = sql.Open("postgres", *dbinfo)
-	check(err)
-	defer db.Close()
-
-	for {
-		err = db.Ping()
-		if err == nil {
-			break
-		}
-		time.Sleep(time.Second)
-	}
-
-	migrations := &migrate.FileMigrationSource{
-		Dir: *migrationsDir,
-	}
-	n, err := migrate.Exec(db, "postgres", migrations, migrate.Up)
-	if err != nil {
-		log.Println(err)
-	} else {
-		log.Printf("Applied %d migrations!\n", n)
-	}
-
-	routes := []Route{
-		{
-			"RecordingsIndex",
-			"GET",
-			"/",
-			index,
-		},
-		{
-			"RecordingCreate",
-			"PUT",
-			"/",
-			create,
-		},
-	}
-	r := NewRouter(routes)
-	lr := handlers.CombinedLoggingHandler(os.Stdout, r)
-	clr := handlers.CORS()(lr)
-
-	log.Printf("Starting rest service on port :%s\n", *port)
-	log.Fatal(http.ListenAndServe(":"+*port, clr))
-}
-
-func check(err error) {
-	if err != nil {
-		panic(err)
-	}
+	http.HandleFunc("/", index)
+	log.Println("Starting rest service on port 80")
+	log.Fatal(http.ListenAndServe(":80", nil))
 }
