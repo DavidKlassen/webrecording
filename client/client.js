f8cd3254d4e1dbeebd7b1c556cb195a2ce91790f
Ivan Bondarenko <ivan.bondarenko@realeyesit.com>
2016-10-13 14:12:34 +0200

    Pipeline refactoring completed

--- client/client.js
+++ client/client.js
@@ -1,29 +1,87 @@
 window.webrecording = {};
-window.webrecording.Recorder = class {
-    constructor(stream) {
+
+window.webrecording.Pipeline = class {
+    constructor(next) {
+        this.next = next;
+    }
+
+    consume(data) {
+        this.next.consume(data);
+    }
+
+    start() {
+        this.next.start();
+    }
+
+    stop() {
+        this.next.stop();
+    }
+};
+
+window.webrecording.Recorder = class extends webrecording.Pipeline {
+    constructor(stream, next) {
+        super(next);
         this.recorder = new MediaRecorder(stream);
-        this.recorder.onstart = () => this.onstart();
         this.recorder.onstop = () => {
-            this.ws.close();
+            super.stop();
             this.onstop();
         };
+        this.recorder.onstart = () => {
+            this.onstart();
+        };
         this.recorder.ondataavailable = e => {
-            this.ws.send(new Blob([e.data]));
-            this.index++;
-            this.chunks.push(e.data);
+            this.next.consume(e.data);
         };
     }
 
     start() {
-        this.index = 0;
-        this.chunks = [];
-        this.ws = new WebSocket("ws://backend.localhost/");
-        this.ws.onopen = () => this.ws.send('test.webm');
-
-        return this.recorder.start(1000);
+        super.start();
+        this.recorder.start(1000);
     }
 
     stop() {
         this.recorder.stop();
     }
+
+    onstart() {
+
+    }
+
+    onstop() {
+
+    }
+
+};
+
+window.webrecording.Uploader = class extends webrecording.Pipeline {
+    constructor() {
+        super();
+        this.guid = (() => {
+            function s4() {
+                return Math.floor((1 + Math.random()) * 0x10000)
+                    .toString(16)
+                    .substring(1);
+            }
+
+            return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+                s4() + '-' + s4() + s4() + s4();
+        })()
+    }
+
+    consume(data) {
+        this.ws.send(new Blob([data]));
+    }
+
+    start() {
+        this.ws = new WebSocket("ws://backend.localhost/");
+        this.ws.onopen = () => this.ws.send(this.guid + '.webm');
+    }
+
+    stop() {
+        this.ws.close();
+    }
+};
+
+window.webrecording.default_pipeline = function (stream) {
+    return new webrecording.Recorder(stream, new webrecording.Uploader());
 };
